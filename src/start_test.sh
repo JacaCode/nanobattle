ba222962673573d2e901d373504850d5dbc856f9
Marcel Rodrigues <marcelgmr@gmail.com>
2015-02-23 16:03:09 -0300

    Inicia servidor mais rapidamente.

--- src/start_test.sh
+++ src/start_test.sh
@@ -4,12 +4,12 @@ N=${1-1}
 
 luajit server.lua $N 2>server.log &
 echo $! > server.pid
-sleep 1
+sleep 0.2
 
 > bots.pid
 for i in `seq 1 $N`
 do
     luajit bot_test.lua $i 2>"bot$i.log" &
     echo $! >> bots.pid
-    sleep 1
+    sleep 0.2
 done
