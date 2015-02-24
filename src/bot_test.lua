e01c7dcd2de3fe0f0fbb973f6a5328613f3b8d64
Marcel Rodrigues <marcelgmr@gmail.com>
2015-02-24 11:39:42 -0300

    Atualiza robô teste.

--- src/bot_test.lua
+++ src/bot_test.lua
@@ -1,6 +1,6 @@
 local client = require "client"
 
-function client.Bot:init()
+function client.Bot:init(w, h, n)
     math.randomseed(os.time() + self.id)
     self.frame = math.random(100)
     self.rot_length = 0
@@ -16,10 +16,10 @@ function client.Bot:turn(bx, by, bd, gd, rd, rv, es)
         self.rot_dir = ({"-", "+"})[math.random(2)]
     end
     self.bot_rot = self.frame < self.rot_length and self.rot_dir or "="
-    self.rad_rot = self.frame % 10 < 2 and "-" or "="
+    self.rad_rot = self.frame % 20 < 2 and "+" or "="
     self.rad_cal = ({"-", "=", "+"})[math.random(3)]
     self.gun_rot = self.frame % 20 < 2 and "+" or "="
-    self.gun_fire = self.frame % 25 == 0 and "+" or "="
+    self.gun_fire = rv > 0 and "+" or "="
     self.frame = (self.frame + 1) % 100
 end
 
