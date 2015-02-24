893ac2dcdf1d5e775a52bdd0179ebcad0e746f2a
Marcel Rodrigues <marcelgmr@gmail.com>
2015-02-23 23:00:44 -0300

    Aumenta aleatoriedade do robô teste.

--- src/bot_test.lua
+++ src/bot_test.lua
@@ -1,6 +1,7 @@
 local client = require "client"
 
 function client.Bot:init()
+    math.randomseed(os.time() + self.id)
     self.frame = math.random(100)
     self.rot_length = 0
     self.rot_dir = "="
@@ -15,7 +16,8 @@ function client.Bot:turn(bx, by, bd, gd, rd, rv, es)
         self.rot_dir = ({"-", "+"})[math.random(2)]
     end
     self.bot_rot = self.frame < self.rot_length and self.rot_dir or "="
-    self.rad_rot = self.frame % 10 < 2 and "+" or "="
+    self.rad_rot = self.frame % 10 < 2 and "-" or "="
+    self.rad_cal = ({"-", "=", "+"})[math.random(3)]
     self.gun_rot = self.frame % 20 < 2 and "+" or "="
     self.gun_fire = self.frame % 25 == 0 and "+" or "="
     self.frame = (self.frame + 1) % 100
