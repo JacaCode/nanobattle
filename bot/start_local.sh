fde3964970a96cfdae9b1b5d4d90cc50175b26d9
Marcel Rodrigues <marcelgmr@gmail.com>
2015-02-25 11:03:20 -0300

    Salva logs em diretório temporário.

--- bot/start_local.sh
+++ bot/start_local.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
 
 N=`wc -l < local.cfg`
+LOG="/tmp/nanolog"
 
-luajit ../src/server.lua $N 2>server.log &
+mkdir -p $LOG
+
+luajit ../src/server.lua $N 2>"$LOG/server.log" &
 echo $! > server.pid
 sleep 0.2
 
@@ -10,7 +13,7 @@ sleep 0.2
 for i in `seq 1 $N`
 do
     read bot
-    luajit "bot_$bot.lua" $i 2>"bot$i.log" &
+    luajit "bot_$bot.lua" $i 2>"$LOG/bot$i.log" &
     echo $! >> bots.pid
     sleep 0.2
 done < local.cfg
