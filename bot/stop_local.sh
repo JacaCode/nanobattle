d63660d79e5ff605ff6c1ee2554134f17727a8bd
Marcel Rodrigues <marcelgmr@gmail.com>
2015-02-25 11:03:59 -0300

    Ignora falhas ao tentar interromper processo.

--- bot/stop_local.sh
+++ bot/stop_local.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 pid=`cat server.pid`
-kill -s INT $pid
+kill -s INT $pid 2> /dev/null
 rm -f server.pid
 
 while read pid
 do
-    kill -s INT $pid
+    kill -s INT $pid 2> /dev/null
 done < bots.pid
 rm -f bots.pid
